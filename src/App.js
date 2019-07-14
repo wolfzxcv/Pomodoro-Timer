e874ae6e037dde7a51de8755b55efc0758ed8dea
Nien <wolfzxcv@gmail.com>
2019-07-14 02:25:12 +0100

    add folders and basic files

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { StylesProvider } from '@material-ui/styles';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './theme/globalStyle';
+import ContextProvider from './context/ContextProvider';
 import StyledComponentsButton from './StyledComponentsButton';
 import theme from './theme/theme';
 
-function App() {
+const App = () => {
   return (
     <StylesProvider injectFirst>
       <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyle />
-          <StyledComponentsButton />
-        </>
+        <ContextProvider>
+          <>
+            <GlobalStyle />
+            <StyledComponentsButton />
+          </>
+        </ContextProvider>
       </ThemeProvider>
     </StylesProvider>
   );
-}
+};
 
 export default App;
